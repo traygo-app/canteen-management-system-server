8ae179fe7ca628d9d9d21b94f0fec71a9b4951ea
Mihai Gurduza <gurduzamihai@gmail.com>
2026-01-22 19:49:27 +0200

    fix: increase sensitive throttling

--- config/settings.py
+++ config/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
         "anon": "5/minute",
         "unverified": "20/minute",
         "verified": "100/minute",
-        "sensitive": "1/minute",
+        "sensitive": "5/minute",
     },
     "NUM_PROXIES": 1,
 }
